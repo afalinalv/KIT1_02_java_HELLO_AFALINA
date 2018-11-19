9cb958303b5e0c99e14fde2170f37fea8fdc347f
afalinalv <afalinalv@ya.ru>
2018-11-19 17:04:18 +0300

    Empty Hello AS 3.3.0-betta4 Gradle 4.10.1 Androidx + java API 28-22

---
+++ app/src/main/java/com/example/kit1_02_java_hello_afalina/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kit1_02_java_hello_afalina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
