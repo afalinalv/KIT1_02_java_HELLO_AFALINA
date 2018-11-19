800142bff67d0616e76759a34786e4c568222ede
afalinalv <afalinalv@ya.ru>
2018-11-19 17:08:29 +0300

    Hello with picture AS 3.3.0-betta4 Gradle 4.10.1 Androidx + java API 28-22

--- app/src/main/java/com/example/kit1_02_java_hello_afalina/MainActivity.java
+++ app/src/main/java/com/example/kit1_02_java_hello_afalina/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.kit1_02_java_hello_afalina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHelloTextView = (TextView)findViewById(R.id.textView);
+        mHelloTextView = findViewById(R.id.textView);
+        mNameEditText = findViewById(R.id.editText);
+
+    }
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+    public void onClick(View view) {
+        if (mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello AFALINA!");
+        } else {
+            mHelloTextView.setText("Привет, " + mNameEditText.getText() );
+        }
     }
 }
